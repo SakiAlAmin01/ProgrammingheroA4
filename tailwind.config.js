41206fb537aa89d7ac99bb0407bb4654176b8b60
Sakialamin01 <sakialaminwork@gmail.com>
2026-02-22 12:18:09 +0600

    Tailwind Css Connect and title and header push

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",   // all your HTML files
+    "./script.js"     // if you use Tailwind classes in JS (rare)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
